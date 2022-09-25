a8c6b1e756d34e4c565717a8862b3accbcba1768
Aaryan Porwal <aaryanporwal2233@gmail.com>
2022-09-25 20:25:52 +0530

    Blocks now spawn on the grid!
    Added initialize() and spawnNewNumber() functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [data, setData] = useState([
@@ -8,6 +8,47 @@ function App() {
     [0, 0, 0, 0],
     [0, 0, 0, 0],
   ]);
+
+  // Functions needed
+  // Initialize
+  const initialize = () => {
+    // deep copy data into newGrid variable
+    let newGrid = JSON.parse(JSON.stringify(data));
+    console.log(newGrid);
+    // Spawn 2 numbers on the grid
+    spawnNewNumber(newGrid);
+    // console.table(newGrid);
+    spawnNewNumber(newGrid);
+    // console.table(newGrid);
+    setData(newGrid);
+  };
+  // spawnNewNumber() -> Spawns a block of 2 or 4 on the board
+  const spawnNewNumber = (newGrid) => {
+    let added = false;
+    let gridFull = false;
+    let attempts = 0;
+    while (!added) {
+      if (gridFull) {
+        break;
+      }
+      let rand1 = Math.floor(Math.random() * 4);
+      let rand2 = Math.floor(Math.random() * 4);
+      attempts++;
+      if (newGrid[rand1][rand2] === 0) {
+        newGrid[rand1][rand2] = Math.random() > 0.5 ? 2 : 4;
+        added = true;
+        // Check if no. already exists
+      }
+    }
+  };
+  // SwipeBlock() -> Left | Right | Up | Down
+  // CheckGameOver()
+  // Reset()
+
+  useEffect(() => {
+    initialize();
+  }, []);
+
   return (
     <div
       style={{
