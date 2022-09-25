0c4fa73176fa1a801906b5005b9f0f7dbd4f003a
Aaryan Porwal <aaryanporwal2233@gmail.com>
2022-09-26 01:55:24 +0530

    Game core logic and styles completed ✅
    Added deepclone.lodash, styles and movement functions. TODO: add gameover, score logic and refactor the ugly code 🤮

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 import { useState, useEffect } from "react";
+import cloneDeep from "lodash.clonedeep";
+import { getColors, useEvent } from "./util";
 
 function App() {
+  // Keycodes for movements
+  const UP_ARROW = 38;
+  const DOWN_ARROW = 40;
+  const LEFT_ARROW = 37;
+  const RIGHT_ARROW = 39;
+
   const [data, setData] = useState([
     [0, 0, 0, 0],
     [0, 0, 0, 0],
@@ -10,10 +18,11 @@ function App() {
   ]);
 
   // Functions needed
+
   // Initialize
   const initialize = () => {
     // deep copy data into newGrid variable
-    let newGrid = JSON.parse(JSON.stringify(data));
+    let newGrid = cloneDeep(data);
     console.log(newGrid);
     // Spawn 2 numbers on the grid
     spawnNewNumber(newGrid);
@@ -22,18 +31,18 @@ function App() {
     // console.table(newGrid);
     setData(newGrid);
   };
+
   // spawnNewNumber() -> Spawns a block of 2 or 4 on the board
   const spawnNewNumber = (newGrid) => {
     let added = false;
     let gridFull = false;
-    let attempts = 0;
     while (!added) {
       if (gridFull) {
         break;
       }
       let rand1 = Math.floor(Math.random() * 4);
       let rand2 = Math.floor(Math.random() * 4);
-      attempts++;
+
       if (newGrid[rand1][rand2] === 0) {
         newGrid[rand1][rand2] = Math.random() > 0.5 ? 2 : 4;
         added = true;
@@ -41,13 +50,219 @@ function App() {
       }
     }
   };
+
   // SwipeBlock() -> Left | Right | Up | Down
+  const swipeLeft = (dummy) => {
+    console.log("swipe left");
+    let oldGrid = data;
+    let newArray = cloneDeep(data);
+
+    for (let i = 0; i < 4; i++) {
+      let board = newArray[i];
+      let slow = 0;
+      let fast = 1;
+      while (slow < 4) {
+        if (fast === 4) {
+          fast = slow + 1;
+          slow++;
+          continue;
+        }
+        if (board[slow] === 0 && board[fast] === 0) {
+          fast++;
+        } else if (board[slow] === 0 && board[fast] !== 0) {
+          board[slow] = board[fast];
+          board[fast] = 0;
+          fast++;
+        } else if (board[slow] !== 0 && board[fast] === 0) {
+          fast++;
+        } else if (board[slow] !== 0 && board[fast] !== 0) {
+          if (board[slow] === board[fast]) {
+            board[slow] = board[slow] + board[fast];
+            board[fast] = 0;
+            fast = slow + 1;
+            slow++;
+          } else {
+            slow++;
+            fast = slow + 1;
+          }
+        }
+      }
+    }
+    if (JSON.stringify(oldGrid) !== JSON.stringify(newArray)) {
+      spawnNewNumber(newArray);
+    }
+    if (dummy) {
+      return newArray;
+    } else {
+      setData(newArray);
+    }
+  };
+
+  const swipeRight = (dummy) => {
+    console.log("swipe right");
+    let oldData = data;
+    let newArray = cloneDeep(data);
+
+    for (let i = 3; i >= 0; i--) {
+      let board = newArray[i];
+      let slow = board.length - 1;
+      let fast = slow - 1;
+      while (slow > 0) {
+        if (fast === -1) {
+          fast = slow - 1;
+          slow--;
+          continue;
+        }
+        if (board[slow] === 0 && board[fast] === 0) {
+          fast--;
+        } else if (board[slow] === 0 && board[fast] !== 0) {
+          board[slow] = board[fast];
+          board[fast] = 0;
+          fast--;
+        } else if (board[slow] !== 0 && board[fast] === 0) {
+          fast--;
+        } else if (board[slow] !== 0 && board[fast] !== 0) {
+          if (board[slow] === board[fast]) {
+            board[slow] = board[slow] + board[fast];
+            board[fast] = 0;
+            fast = slow - 1;
+            slow--;
+          } else {
+            slow--;
+            fast = slow - 1;
+          }
+        }
+      }
+    }
+    if (JSON.stringify(newArray) !== JSON.stringify(oldData)) {
+      spawnNewNumber(newArray);
+    }
+    if (dummy) {
+      return newArray;
+    } else {
+      setData(newArray);
+    }
+  };
+
+  const swipeDown = (dummy) => {
+    console.log("swipe down");
+    console.log(data);
+    let board = cloneDeep(data);
+    let oldData = JSON.parse(JSON.stringify(data));
+    for (let i = 3; i >= 0; i--) {
+      let slow = board.length - 1;
+      let fast = slow - 1;
+      while (slow > 0) {
+        if (fast === -1) {
+          fast = slow - 1;
+          slow--;
+          continue;
+        }
+        if (board[slow][i] === 0 && board[fast][i] === 0) {
+          fast--;
+        } else if (board[slow][i] === 0 && board[fast][i] !== 0) {
+          board[slow][i] = board[fast][i];
+          board[fast][i] = 0;
+          fast--;
+        } else if (board[slow][i] !== 0 && board[fast][i] === 0) {
+          fast--;
+        } else if (board[slow][i] !== 0 && board[fast][i] !== 0) {
+          if (board[slow][i] === board[fast][i]) {
+            board[slow][i] = board[slow][i] + board[fast][i];
+            board[fast][i] = 0;
+            fast = slow - 1;
+            slow--;
+          } else {
+            slow--;
+            fast = slow - 1;
+          }
+        }
+      }
+    }
+    if (JSON.stringify(board) !== JSON.stringify(oldData)) {
+      spawnNewNumber(board);
+    }
+    if (dummy) {
+      return board;
+    } else {
+      setData(board);
+    }
+  };
+
+  const swipeUp = (dummy) => {
+    console.log("swipe up");
+    let board = cloneDeep(data);
+    let oldData = JSON.parse(JSON.stringify(data));
+    for (let i = 0; i < 4; i++) {
+      let slow = 0;
+      let fast = 1;
+      while (slow < 4) {
+        if (fast === 4) {
+          fast = slow + 1;
+          slow++;
+          continue;
+        }
+        if (board[slow][i] === 0 && board[fast][i] === 0) {
+          fast++;
+        } else if (board[slow][i] === 0 && board[fast][i] !== 0) {
+          board[slow][i] = board[fast][i];
+          board[fast][i] = 0;
+          fast++;
+        } else if (board[slow][i] !== 0 && board[fast][i] === 0) {
+          fast++;
+        } else if (board[slow][i] !== 0 && board[fast][i] !== 0) {
+          if (board[slow][i] === board[fast][i]) {
+            board[slow][i] = board[slow][i] + board[fast][i];
+            board[fast][i] = 0;
+            fast = slow + 1;
+            slow++;
+          } else {
+            slow++;
+            fast = slow + 1;
+          }
+        }
+      }
+    }
+    if (JSON.stringify(oldData) !== JSON.stringify(board)) {
+      spawnNewNumber(board);
+    }
+    if (dummy) {
+      return board;
+    } else {
+      setData(board);
+    }
+  };
+
+  // Handle Key Down
+  const handleKeyDown = (event) => {
+    switch (event.keyCode) {
+      case UP_ARROW:
+        swipeUp();
+        break;
+      case DOWN_ARROW:
+        swipeDown();
+        break;
+      case RIGHT_ARROW:
+        swipeRight();
+        break;
+      case LEFT_ARROW:
+        swipeLeft();
+        break;
+      default:
+        break;
+    }
+  };
+
   // CheckGameOver()
   // Reset()
 
   useEffect(() => {
     initialize();
+    // document.addEventListener("keydown", handleKeyDown);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useEvent("keydown", handleKeyDown);
 
   return (
     <div
@@ -81,10 +296,11 @@ const Block = ({ num }) => {
     <div
       style={{
         ...blockStyle,
+        background: getColors(num),
         color: num === 2 || num === 4 ? "#645B52" : "#F7F4EF",
       }}
     >
-      {num}
+      {num !== 0 ? num : ""}
     </div>
   );
 };
